0c7f1174d9dd17c5aac99166480ba04ada1111f4
infectioni <infectioni@mail.ru>
2013-03-26 21:15:12 +0300

    merge

--- src/Ctrl-Space/Game1.cs
+++ src/Ctrl-Space/Game1.cs
@@ -141,6 +141,8 @@ namespace Ctrl_Space
                 }
             }
 
+            Collisions.Detect(_asteroids);
+
             base.Update(gameTime);
         }
 
@@ -176,7 +178,7 @@ namespace Ctrl_Space
                 (_gamePadState.IsButtonDown(Buttons.A) && _oldGamePadState.IsButtonUp(Buttons.A)))
             {
                 var kickRocket = 40f;
-                var speedRocket = 0.9f;
+                var speedRocket = 4.9f;
 
                 GameObject rocketAsteroid = new GameObject(10,
                     new Vector2(_ship.Position.X + (float)(kickRocket * Math.Sin(_ship.Rotation)), _ship.Position.Y - (float)(kickRocket * Math.Cos(_ship.Rotation))),
