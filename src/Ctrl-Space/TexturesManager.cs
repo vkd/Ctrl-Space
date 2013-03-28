f4d19f41be7fde00ced04f958c80fd61810bf59c
Dmitry Kuznetsov <deelayka@gmail.com>
2013-03-29 00:02:03 +0400

    Better gfx

--- src/Ctrl-Space/TexturesManager.cs
+++ src/Ctrl-Space/TexturesManager.cs
@@ -10,14 +10,16 @@ namespace Ctrl_Space
     public class TexturesManager
     {
         public Texture2D ShipTexture { get; set; }
+        public Texture2D ShipOffTexture { get; set; }
         public Texture2D RocketTexture { get; set; }
         public Texture2D AsteroidTexture { get; set; }
         public Texture2D SpeedBonusTexture { get; set; }
 
         public TexturesManager(ContentManager contentManaget)
         {
-            ShipTexture = contentManaget.Load<Texture2D>("Bitmap1");
-            AsteroidTexture = contentManaget.Load<Texture2D>("Bitmap2");
+            ShipTexture = contentManaget.Load<Texture2D>("Ship");
+            ShipOffTexture = contentManaget.Load<Texture2D>("Ship-off");
+            AsteroidTexture = contentManaget.Load<Texture2D>("Asteroid");
             RocketTexture = contentManaget.Load<Texture2D>("Rocket");
             SpeedBonusTexture = contentManaget.Load<Texture2D>("SpeedBonus");
         }
