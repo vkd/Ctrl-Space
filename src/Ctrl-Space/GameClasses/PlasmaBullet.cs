da021011a5c319dc8c9667df6727f7429f6e9458
deelayka <deelayka@gmail.com>
2013-04-09 13:59:32 +0400

    Update PlasmaBullet.cs

--- src/Ctrl-Space/GameClasses/PlasmaBullet.cs
+++ src/Ctrl-Space/GameClasses/PlasmaBullet.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +9,19 @@ namespace Ctrl_Space
 {
     class PlasmaBullet : GameObject
     {
+        private float State = 1.0f;
+
         public override Texture2D GetTexture()
         {
             return TextureManager.PlasmaBulletTexture;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            State -= .01f;
+            if (State < 0f)
+                IsDestroyed = true;
         }
     }
 }
