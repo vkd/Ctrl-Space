61d7b3360405ab75071a6f3378bd09cc34f2af5a
Dmitry Kuznetsov <deelayka@gmail.com>
2013-04-14 23:10:41 +0400

    Precise collizions

--- src/Ctrl-Space/Physics/Collisions.cs
+++ src/Ctrl-Space/Physics/Collisions.cs
@@ -27,7 +27,7 @@ namespace Ctrl_Space
                             for (int j = 0; j < clusters[wj, wi].Count; j++)
                                 for (int i = 0; i < clusters[cluj, clui].Count; i++)
                                 {
-                                    if (i == j || i < 0 || j < 0) continue;
+                                    //if (i == j || i < 0 || j < 0) continue;
                                     var go1 = clusters[cluj, clui][i];
                                     var go2 = clusters[wj, wi][j];
                                     float dx = go2.Position.X - go1.Position.X - fx;
@@ -45,54 +45,61 @@ namespace Ctrl_Space
                                     float t2 = (-b - Maf.Sqrt(disc)) / (2f * a);
 
                                     float ol2 = rr * rr - dx * dx - dy * dy;
-                                    //if ((t1 <= 0 && t1 > -1) || (t2 <= 0 && t2 > -1))
-                                    if (ol2 > 0)
+                                    if ((t1 <= 0 && t1 > -1) || (t2 <= 0 && t2 > -1))
+                                    //if (ol2 > 0)
                                     {
-                                        if ((go1 is Rocket || go1 is PlasmaBullet) && (go2 is Asteroid || go2 is Ship))
-                                        {
-                                            for (int h = 0; h < 100; h++)
-                                                particles.Emit(particleParameters, (go1.Position + go2.Position) / 2f, 3f * Chaos.GetFloat() * Chaos.GetVector2());
-                                            gameObjects.Remove(go1);
-                                            if (go2 is Asteroid)
-                                            {
-                                                go2.Size -= 20f;
-                                                if (go2.Size < 40f)
-                                                    gameObjects.Remove(go2);
-                                            }
-                                            continue;
-                                        }
+                                        particles.Emit(particleParameters, ((go1.Position + new Vector2(fx, fy)) * go2.Size + go2.Position * go1.Size) / (go1.Size + go2.Size), Vector2.Zero);
+                                        //particles.Emit(particleParameters, go1.Position, Vector2.Zero);
+                                        //particles.Emit(particleParameters, go2.Position, Vector2.Zero);
+                                        //if ((go1 is Rocket || go1 is PlasmaBullet) && (go2 is Asteroid || go2 is Ship))
+                                        //{
+                                        //    for (int h = 0; h < 100; h++)
+                                        //        particles.Emit(particleParameters, (go1.Position + go2.Position) / 2f, 3f * Chaos.GetFloat() * Chaos.GetVector2());
+                                        //    gameObjects.Remove(go1);
+                                        //    if (go2 is Asteroid)
+                                        //    {
+                                        //        go2.Size -= 20f;
+                                        //        if (go2.Size < 40f)
+                                        //            gameObjects.Remove(go2);
+                                        //    }
+                                        //    continue;
+                                        //}
 
-                                        if ((go1 is Ship) && go2 is SpeedBonus)
-                                        {
-                                            go1.Speed += new Vector2(10f * Maf.Sin(go1.Rotation), -10f * Maf.Cos(go1.Rotation));
-                                            gameObjects.Remove(go2);
-                                            continue;
-                                        }
+                                        //if ((go1 is Ship) && go2 is SpeedBonus)
+                                        //{
+                                        //    go1.Speed += new Vector2(10f * Maf.Sin(go1.Rotation), -10f * Maf.Cos(go1.Rotation));
+                                        //    gameObjects.Remove(go2);
+                                        //    continue;
+                                        //}
 
-                                        // ось столкновения и нормаль к ней
-                                        var nrm = new Vector2(dx, dy);
-                                        var tan = new Vector2(dy, -dx);
-                                        nrm.Normalize();
-                                        tan.Normalize();
+                                        //if (ol2 > 0)
+                                        //{
 
-                                        // проекция скорости на ось столкновения (нормальная скорость)
-                                        float go1nrm = Vector2.Dot(go1.Speed, nrm);
-                                        float go2nrm = Vector2.Dot(go2.Speed, nrm);
+                                        //    // ось столкновения и нормаль к ней
+                                        //    var nrm = new Vector2(dx, dy);
+                                        //    var tan = new Vector2(dy, -dx);
+                                        //    nrm.Normalize();
+                                        //    tan.Normalize();
 
-                                        // перераспределяем импульс между нормальными скоростями в соответствии с массами
-                                        float go1rsp = ((go1.Mass - go2.Mass) * go1nrm + 2f * go2.Mass * go2nrm) / (go1.Mass + go2.Mass);
-                                        float go2rsp = ((go2.Mass - go1.Mass) * go2nrm + 2f * go1.Mass * go1nrm) / (go1.Mass + go2.Mass);
+                                        //    // проекция скорости на ось столкновения (нормальная скорость)
+                                        //    float go1nrm = Vector2.Dot(go1.Speed, nrm);
+                                        //    float go2nrm = Vector2.Dot(go2.Speed, nrm);
 
-                                        // проекция скорости на нормаль к оси столкновения (тангенциальная скорость)
-                                        float go1tan = Vector2.Dot(go1.Speed, tan);
-                                        float go2tan = Vector2.Dot(go2.Speed, tan);
+                                        //    // перераспределяем импульс между нормальными скоростями в соответствии с массами
+                                        //    float go1rsp = ((go1.Mass - go2.Mass) * go1nrm + 2f * go2.Mass * go2nrm) / (go1.Mass + go2.Mass);
+                                        //    float go2rsp = ((go2.Mass - go1.Mass) * go2nrm + 2f * go1.Mass * go1nrm) / (go1.Mass + go2.Mass);
 
-                                        go1.Speed = nrm * go1rsp + tan * go1tan;
-                                        go2.Speed = nrm * go2rsp + tan * go2tan;
+                                        //    // проекция скорости на нормаль к оси столкновения (тангенциальная скорость)
+                                        //    float go1tan = Vector2.Dot(go1.Speed, tan);
+                                        //    float go2tan = Vector2.Dot(go2.Speed, tan);
 
-                                        float gp = Maf.Sqrt(ol2);
-                                        go1.Position -= gp * go2.Mass / (go1.Mass + go2.Mass) * nrm;
-                                        go2.Position += gp * go1.Mass / (go1.Mass + go2.Mass) * nrm;
+                                        //    go1.Speed = nrm * go1rsp + tan * go1tan;
+                                        //    go2.Speed = nrm * go2rsp + tan * go2tan;
+
+                                        //    float gp = Maf.Sqrt(ol2);
+                                        //    go1.Position -= gp * go2.Mass / (go1.Mass + go2.Mass) * nrm;
+                                        //    go2.Position += gp * go1.Mass / (go1.Mass + go2.Mass) * nrm;
+                                        //}
                                     }
                                 }
                         }
