b59146d9151a6538a578ed35e00a03904f07e4b0
Dmitry Kuznetsov <deelayka@gmail.com>
2013-09-27 23:36:41 +0400

    Lost file

---
+++ src/Ctrl-Space/Graphics/ParticlePool.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+
+namespace Ctrl_Space.Graphics
+{
+    class ParticlePool
+    {
+        private ConcurrentBag<Particle> _particles;
+
+        public ParticlePool()
+        {
+            _particles = new ConcurrentBag<Particle>();
+        }
+
+        public Particle GetParticle(ParticleParameters particleParameters)
+        {
+            Particle particle;
+            if (_particles.TryTake(out particle))
+            {
+                particle.Reset(particleParameters);
+                return particle;
+            }
+            return new Particle(particleParameters);
+        }
+
+        public void PutParticle(Particle particle)
+        {
+            _particles.Add(particle);
+        }
+    }
+}
