0e969883d9b2782dd856d906f587e56df6b02e0f
Dmitry Kuznetsov <deelayka@gmail.com>
2013-09-30 22:07:17 +0400

    Less garbage in XNAKeyboard

--- src/Ctrl-Space/Input/Devices/XNAKeyboard.cs
+++ src/Ctrl-Space/Input/Devices/XNAKeyboard.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Input;
 
 namespace Ctrl_Space.Input.Devices
 {
     class XNAKeyboard : IDevice
     {
-        private List<DeviceData> _deviceData;
+        private readonly List<DeviceData> _deviceData;
+        private readonly Keys[] _keys;
+        private readonly DeviceData[] _datas;
 
         public XNAKeyboard(Game game)
         {
+            var allKeys = Enum.GetValues(typeof(Keys));
+            _keys = new Keys[allKeys.Length];
+            _datas = new DeviceData[allKeys.Length];
+            for (int k = 0; k < allKeys.Length; k++)
+            {
+                _keys[k] = (Keys)allKeys.GetValue(k);
+                _datas[k] = new DeviceData() { Device = Name, Event = Enum.GetName(typeof(Keys), _keys[k]), Data = 1.0f };
+            }
             _deviceData = new List<DeviceData>();
         }
 
-        public string Name = "XNAKeyboard";
+        public readonly string Name = "XNAKeyboard";
 
         public IEnumerable<DeviceData> GetData()
         {
@@ -20,8 +31,9 @@ namespace Ctrl_Space.Input.Devices
 
             var state = Keyboard.GetState();
             var keys = state.GetPressedKeys();
-            foreach (var key in keys)
-                _deviceData.Add(new DeviceData() { Device = Name, Event = key.ToString(), Data = 1.0f });
+            for (int i = 0; i < _keys.Length; i++)
+                if (state.IsKeyDown(_keys[i]))
+                    _deviceData.Add(_datas[i]);
             return _deviceData;
         }
     }
