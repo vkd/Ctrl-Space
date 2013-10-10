56d14dd10bccb099fd5caa1fb3d1d7404b832061
Dmitry Kuznetsov <deelayka@gmail.com>
2013-10-11 00:23:06 +0400

    Garbageless int to string

--- src/Ctrl-Space/Helpers/AwesomeStringBuilder.cs
+++ src/Ctrl-Space/Helpers/AwesomeStringBuilder.cs
@@ -7,6 +7,7 @@ namespace Ctrl_Space.Helpers
         private readonly char[] _string;
         private int _position;
         private readonly int _capacity;
+        private readonly char[] _digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
         public AwesomeStringBuilder(int capacity)
         {
@@ -27,11 +28,31 @@ namespace Ctrl_Space.Helpers
             return this;
         }
 
-        //public AwesomeStringBuilder Append(int value)
-        //{
-        //    _string.Append(value);
-        //    return this;
-        //}
+        public AwesomeStringBuilder Append(int value)
+        {
+            bool _negative = value < 0;
+            if (_negative)
+                value = -value;
+            int begin = _position;
+            do
+            {
+                if (_position >= _capacity)
+                    break;
+                _string[_position++] = _digits[value % 10];
+                value /= 10;
+            }
+            while (value != 0);
+            if (_position < _capacity && _negative)
+                _string[_position++] = '-';
+            int end = _position;
+            while (begin < end)
+            {
+                char t = _string[begin];
+                _string[begin++] = _string[--end];
+                _string[end] = t;
+            }
+            return this;
+        }
 
         public void AppendToStringBuilder(StringBuilder stringBuilder)
         {
