d59b08d5b0ca5d47c68aa5c59e4d9132ccc08c18
vkd <vkd.castle@gmail.com>
2015-01-05 11:51:22 +0300

    Freeze on center

--- src/Ctrl-Space/Input/Devices/XNAMouse.cs
+++ src/Ctrl-Space/Input/Devices/XNAMouse.cs
@@ -6,7 +6,10 @@ namespace Ctrl_Space.Input.Devices
 {
     class XNAMouse : IDevice
     {
+        public readonly bool FreezeOnCenter = true;
+
         private Point _center;
+        private Point _lastPosition;
 
         public XNAMouse(Game game)
         {
@@ -17,8 +20,11 @@ namespace Ctrl_Space.Input.Devices
             _dataY = new DeviceData() { Device = Name, Event = "Y", Data = 0.0f };
 
             _center = new Point(game._graphics.GraphicsDevice.Viewport.Width / 2, game._graphics.GraphicsDevice.Viewport.Height / 2);
-            game.IsMouseVisible = false;
-            Mouse.SetPosition(_center.X, _center.Y);
+            if (FreezeOnCenter)
+            {
+                Mouse.SetPosition(_center.X, _center.Y);
+            }
+            _lastPosition = Mouse.GetState().Position;
         }
 
         public readonly string Name = "XNAMouse";
@@ -32,7 +38,6 @@ namespace Ctrl_Space.Input.Devices
         public void GetData(List<DeviceData> data)
         {
             var state = Mouse.GetState();
-            Mouse.SetPosition(_center.X, _center.Y);
 
             if (state.LeftButton == ButtonState.Pressed)
                 data.Add(_dataLMB);
@@ -40,16 +45,22 @@ namespace Ctrl_Space.Input.Devices
                 data.Add(_dataMMB);
             if (state.RightButton == ButtonState.Pressed)
                 data.Add(_dataRMB);
-            if (state.X - _center.X != 0)
+            if (state.X - _lastPosition.X != 0)
             {
-                _dataX.Data = (state.X - _center.X) * .02f;
+                _dataX.Data = (state.X - _lastPosition.X) * .02f;
                 data.Add(_dataX);
             }
-            if (state.Y - _center.Y != 0)
+            if (state.Y - _lastPosition.Y != 0)
             {
-                _dataY.Data = (state.Y - _center.Y) * .02f;
+                _dataY.Data = (state.Y - _lastPosition.Y) * .02f;
                 data.Add(_dataY);
             }
+
+            if (FreezeOnCenter)
+            {
+                Mouse.SetPosition(_center.X, _center.Y);
+            }
+            _lastPosition = Mouse.GetState().Position;
         }
     }
 }
